3795ae15c2c9eab98481db455d6bf3a27e568b91
dreamsproxy <changfengjui@gmail.com>
2023-12-27 02:09:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,15 @@ import plotly.express as px
 def load_dataset():
     df = pd.read_csv('winequality-white.csv', delimiter=';')
     return df
+df = load_dataset()
+def get_correlation_heatmap(df):
+    correlation = df.corr()
 
-st.title("White Wine Quality Prediction")
-st.write("White Wine Quality Prediction")
-st.line_chart()
-
-def get_correlation_heatmap():
-
-    z = [[.1, .3, .5, .7, .9],
-         [1, .8, .6, .4, .2],
-         [.2, 0, .5, .7, .9],
-         [.9, .8, .4, .2, 0],
-         [.3, .4, .5, .7, 1]]
-
-    fig = px.imshow(z, text_auto=True)
+    fig = px.imshow(correlation, text_auto=True)
     st.plotly_chart(fig, theme="streamlit")
 
-get_correlation_heatmap()
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+st.title("White Wine Quality Prediction")
+
+get_correlation_heatmap(df)
+
+st.line_chart()
