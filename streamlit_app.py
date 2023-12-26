3a2f6ca4178ebfb1618dcb65a6adac1bf740a15c
dreamsproxy <changfengjui@gmail.com>
2023-12-27 02:16:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,17 @@ def load_dataset():
     df = pd.read_csv('winequality-white.csv', delimiter=';')
     return df
 df = load_dataset()
+
 def get_correlation_heatmap(df):
     correlation = df.corr()
-
     fig = px.imshow(correlation, title = "Correlation Heatmap",
                     text_auto=True,
                     width=800, height=800, aspect="equal")
     st.plotly_chart(fig, theme="streamlit")
 
-st.title("White Wine Quality Prediction")
+if __name__ == "__main__":
+    st.title("White Wine Quality Prediction")
+    st.write()
+    get_correlation_heatmap(df)
 
-st.write()
-get_correlation_heatmap(df)
-
-st.line_chart()
+    st.line_chart()
