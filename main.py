47815ab4a245937a6e17960dc858c96638166463
dreamsproxy <changfengjui@gmail.com>
2023-12-27 11:31:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
-
+import seaborn as sns
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
-
 import tensorflow as tf
 
+scaler = StandardScaler()
+encoder = LabelEncoder()
 data = pd.read_csv('winequality-white.csv', delimiter=';')
 
 corr = data.corr()
@@ -16,3 +16,52 @@ corr = data.corr()
 plt.figure(figsize=(12, 10))
 sns.heatmap(corr, annot=True, vmin=-1.0, vmax=1.0)
 plt.show()
+
+data['quality'] = encoder.fit_transform(data['quality'])
+Y = data['quality']
+X = data.drop('quality', axis=1)
+
+
+X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=42)
+
+num_features = X.shape[1]
+num_classes = len(Y.unique())
+
+inputs = tf.keras.Input(shape=(num_features,))
+x = tf.keras.layers.Dense(128, activation='relu')(inputs)
+
+x = tf.keras.layers.Dense(128, activation='relu')(x)
+x = tf.keras.layers.BatchNormalization()(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+
+x = tf.keras.layers.Dense(64, activation='relu')(x)
+x = tf.keras.layers.BatchNormalization()(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+
+x = tf.keras.layers.Dense(32, activation='relu')(x)
+x = tf.keras.layers.BatchNormalization()(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+
+outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+batch_size = 32
+epochs = 100
+
+history = model.fit(
+    X_train,
+    y_train,
+    validation_split=0.2,
+    batch_size=batch_size,
+    epochs=epochs,
+    callbacks=[tf.keras.callbacks.ReduceLROnPlateau()]
+)
