dd35697aa332e404d2c60dec3e4d637e4675351d
dreamsproxy <changfengjui@gmail.com>
2023-12-27 16:31:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ def load_dataset(synthetic = True):
         df = pd.read_csv('tabular-actgan-SYNTHETIC.csv', delimiter=';')
     else:
         df = pd.read_csv("winequality-white.csv", delimiter=";")
-    g = data.groupby('quality')
+    g = df.groupby('quality')
     g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True))
-    data = pd.DataFrame(g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True)))
+    df = pd.DataFrame(g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True)))
     df["quality"] = encoder.fit_transform(df["quality"])
     y = df["quality"]
     x = df.drop("quality", axis=1)
