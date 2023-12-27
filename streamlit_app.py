77d0afef69e2279f5eabc0904d78ed3984696f47
dreamsproxy <changfengjui@gmail.com>
2023-12-27 17:52:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ encoder = LabelEncoder()
 def load_dataset(synthetic = True):
     if synthetic:
         df = pd.read_csv('synthetic_dataset.csv', delimiter=',')
-        df = df.drop(0, axis=1)
+        df = df.drop('index', axis=1)
         print(df)
     else:
         df = pd.read_csv("winequality-white.csv", delimiter=";")
