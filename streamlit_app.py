ef62d576f027384a16d5fcd3b63eca96fc14a52f
dreamsproxy <changfengjui@gmail.com>
2023-12-27 21:01:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,11 +67,9 @@ if __name__ == "__main__":
         
         param_grid = {
             "max_features": ["sqrt", "log"],  
-            "min_samples_split": [1, 3, 5, 10],  
-            "min_samples_leaf": [1, 2],  
             "n_estimators": [32, 64, 128, 256]}
         
-        model = GridSearchCV(rfc, param_grid=param_grid, cv = 5, n_jobs=4)
+        model = GridSearchCV(rfc, param_grid=param_grid, cv = 5, n_jobs=5)
         print("Search done.")
         model.fit(x_train, y_train)
         print("Fit done.")
